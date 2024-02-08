3607c1fc7b6e335194eae98cc14f6ae004a896ab
tomoki-nishinaga <tomoki.2000.62@icloud.com>
2024-02-08 23:49:14 +0900

    TweetInputをMUIコンポーネントで実装

--- src/features/components/TweetInput.tsx
+++ src/features/components/TweetInput.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useRef } from "react";
 import { tweetProps } from "../Tweet";
+import { Box, Button, TextField } from "@mui/material";
 
 export type tweetInputProps = {
   addTweet: (tweet: tweetProps) => void;
@@ -9,30 +10,36 @@ export const TweetInput = ({ addTweet }: tweetInputProps): JSX.Element => {
   //要素にアクセスするための参照を取得
   // ref.currentのデフォルト値はnullにしておく
   // 参照したい要素にJSXの方でこのrefをセットしてする
-  const textareaRef = useRef(null);
+  const textareaRef = useRef<HTMLInputElement>(null);
 
   const sendTweet = useCallback(() => {
     if (textareaRef.current) {
+      console.log(textareaRef);
       addTweet({
         id: new Date().getTime(),
         icon: "☠",
         displayName: "ミスター死",
         accountName: "mrdeath",
-        content: textareaRef.current,
+        content: textareaRef.current.value,
       });
     }
   }, [addTweet]);
 
   return (
-    <div>
-      <div>
-        <textarea className="tweet-textarea" ref={textareaRef}></textarea>
-      </div>
-      <div>
-        <button onClick={sendTweet} className="send-tweet">
+    <Box>
+      <Box>
+        <TextField
+          className="tweet-textarea"
+          inputRef={textareaRef}
+          rows={4}
+          multiline
+        />
+      </Box>
+      <Box>
+        <Button className="send-tweet" variant="outlined" onClick={sendTweet}>
           Tweet
-        </button>
-      </div>
-    </div>
+        </Button>
+      </Box>
+    </Box>
   );
 };
