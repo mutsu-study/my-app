67bc924ccb20dae3a8225b7c2af8550bb0cf959a
tomoki-nishinaga <tomoki.2000.62@icloud.com>
2024-02-12 23:14:51 +0900

    プロトタイプ完成

--- src/features/components/TweetInput.tsx
+++ src/features/components/TweetInput.tsx
@@ -4,6 +4,7 @@ import {
   Box,
   Button,
   FormControl,
+  Grid,
   InputAdornment,
   InputLabel,
   MenuItem,
@@ -85,57 +86,69 @@ export const TweetInput = ({ addTweet }: tweetInputProps): JSX.Element => {
 
   return (
     <Box>
-      <form onSubmit={handleSubmit(sendTweet)}>
-        <Stack>
+      <Stack>
+        <form onSubmit={handleSubmit(sendTweet)}>
+          <Stack>
+            <Grid container>
+              <Grid item xs={2}>
+                <Box sx={{ mr: 1 }}>
+                  <FormControl className="iconSelect" variant="filled">
+                    <InputLabel className="iconSelect">icon</InputLabel>
+                    <Select
+                      {...register("icon")}
+                      sx={{ mr: 1 }}
+                      value={icon}
+                      onChange={handleSelectIcon}
+                    >
+                      {iconList.map((icon) => (
+                        <MenuItem key={icon.iconId} value={icon.displayIcon}>
+                          {icon.displayIcon}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Grid>
+              <Grid item xs={3} className="displayName" sx={{ mr: 2 }}>
+                <Box className="displayName">
+                  <TextField
+                    {...register("displayName")}
+                    label="displayName"
+                    variant="filled"
+                    inputRef={displayNameRef}
+                    sx={{ width: "100%" }}
+                  />
+                </Box>
+              </Grid>
+              <Grid item xs={4}>
+                <Box className="accountName" sx={{ mr: 2 }}>
+                  <TextField
+                    {...register("accountName")}
+                    label="accontName"
+                    variant="filled"
+                    inputRef={accontNameRef}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">@</InputAdornment>
+                      ),
+                    }}
+                    sx={{ width: "100%" }}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+          </Stack>
           <Box>
-            <FormControl className="iconSelect" variant="filled">
-              <InputLabel className="iconSelect">icon</InputLabel>
-              <Select
-                {...register("icon")}
-                sx={{ mr: 1 }}
-                value={icon}
-                onChange={handleSelectIcon}
-              >
-                {iconList.map((icon) => (
-                  <MenuItem key={icon.iconId} value={icon.displayIcon}>
-                    {icon.displayIcon}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
             <TextField
-              {...register("displayName")}
-              label="displayName"
-              variant="filled"
-              inputRef={displayNameRef}
-              sx={{
-                mr: 1,
-              }}
-            />
-            <TextField
-              {...register("accountName")}
-              label="accontName"
-              variant="filled"
-              inputRef={accontNameRef}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">@</InputAdornment>
-                ),
-              }}
-              sx={{ mr: 1 }}
+              {...register("content")}
+              className="tweet-textarea"
+              rows={4}
+              multiline
+              inputRef={tweetRef}
+              sx={{ mt: 1 }}
             />
           </Box>
-        </Stack>
-        <Box>
-          <TextField
-            {...register("content")}
-            className="tweet-textarea"
-            rows={4}
-            multiline
-            inputRef={tweetRef}
-            sx={{ mt: 1 }}
-          />
-        </Box>
+        </form>
         <Box>
           <Button
             className="send-tweet"
@@ -147,7 +160,7 @@ export const TweetInput = ({ addTweet }: tweetInputProps): JSX.Element => {
             Tweet
           </Button>
         </Box>
-      </form>
+      </Stack>
     </Box>
   );
 };
